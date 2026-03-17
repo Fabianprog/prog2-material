439655188a18a943cc0bf8594c6f88570ae46f4f
Fabian <jobeliusf@gmail.com>
2026-03-17 09:44:09 +0100

    h

---
+++ src/test/java/algods/lecture/vl_2026_03_12/ListsData/ListsData/Data.java
@@ -0,0 +1,12 @@
+package algods.lecture.vl_2026_03_12.ListsData;
+
+public class Data {
+    public int time;
+    public int temp;
+
+    public Data(int time, int temp) {
+        this.time = time;
+        this.temp = temp;
+    }
+    
+}
